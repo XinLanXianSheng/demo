a06b26766761ef61156eef2dd336a8485921006b
XinLanXianSheng <41823079+XinLanXianSheng@users.noreply.github.com>
2019-04-10 00:13:48 +0800

    Update TestController.java

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -3,6 +3,6 @@ package com.example.demo.controller;
 public class TestController {
 
 	public static void main(String[] args) {
-		System.out.println("hahass");
+		System.out.println("error");
 	}
 }
